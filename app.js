ce8e84256b5eac740f333b36f99c57804cfb2fba
Jacob Root <otis@otisroot.com>
2023-02-17 16:39:20 -0800

    Add dotenv

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// Function to get a subcomponent of newsletter
+function getNewsData() {
+
+}
+
+// Function to get full newsletter stuff
+
+
+app.get('/', (req, res) => {
+  res.send("Result");
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
